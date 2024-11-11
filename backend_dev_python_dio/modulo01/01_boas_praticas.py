2cb8be16f1c6cf53d08cd07dbf3cb82e955f2a60
nunescode <nunes.adsi@gmail.com>
2024-11-11 14:28:57 -0300

    Backend Python

---
+++ backend_dev_python_dio/modulo01/01_boas_praticas.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+frutas = [
+    "pera",
+    "maçã",
+    "uva",
+    "abacate",
+    "abacate",
+    "abacate",
+    "abacate",
+    "abacate",
+    "abacate",
+]
+
+a = "sadasdniolqwdeQWEOQEOQLWMN"
+b = "ASDOADEQWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+c = ["asd", "deded"]
---
+++ backend_dev_python_dio/modulo01/exemplos.py
@@ -0,0 +1,5 @@
+def somar(argumento_1, argumento_2):
+    pass
+
+class CasaPropria:
+    pass
